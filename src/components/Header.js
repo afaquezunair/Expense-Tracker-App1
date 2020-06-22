f5c3a60b0d82391503486abdfa23875f89816352
afaquezunair <59284618+afaquezunair@users.noreply.github.com>
2020-06-22 19:12:15 +0500

    Final

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker by Adil Altaf
+            Expense Tracker by Afaque Zunair
         </h2>
     )
 }
